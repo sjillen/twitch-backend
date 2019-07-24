bbe91dea383689cbc2c58f5ab34cb459dfe2d3a0
Thomas Proust <thomas.proust89@gmail.com>
2019-07-24 07:36:05 +0200

    setup mongo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('dotenv').config();
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
@@ -11,7 +12,7 @@ const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json({type: '*/*'}));
+app.use(bodyParser.json({type: 'application/json'}));
 
 require('./router')(app);
 
